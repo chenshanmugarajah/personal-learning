e1ea3239f4f09a0b0c47c5b4706f28673a934835
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-08-04 19:20:16 +0100

    implemented selection sort

---
+++ Sorting/01_selection_sort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_selection_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> prices = new List<int>()
+            {
+                314, 454, 656, 123, 454, 676, 342, 767, 111, 11123
+            };
+
+            Console.WriteLine("old list");
+            foreach (var number in prices)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("");
+
+            int smallest, temp;
+
+            for (int i = 0; i < prices.Count; i++)
+            {
+                smallest = i;
+
+                for (int j = i+1; j < prices.Count; j++)
+                {
+                    if (prices[j] < prices[smallest]) smallest = j;
+                }
+                temp = prices[smallest];
+                prices[smallest] = prices[i];
+                prices[i] = temp;
+            }
+
+            Console.WriteLine("new list");
+            foreach (var number in prices)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
